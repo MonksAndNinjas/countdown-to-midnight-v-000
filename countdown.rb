042e48844fc637cdc0c1e33123f41ab200f9382f
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-16 19:08:38 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,13 @@
 #write your code here
 def countdown(x)
   while x > 0
-    countdown_with_sleep(x)
+    countdown_with_sleep(s)
     puts "#{x} SECOND(S)!"
     x -= 1
   end
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(x)
-  sleep(x)
+def countdown_with_sleep(s)
+  sleep(s)
 end
